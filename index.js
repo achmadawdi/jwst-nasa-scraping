1357533e866dcc576f828debca0de34c94ac0b60
achmadawdi <achmadawdi99@gmail.com>
2022-01-07 04:21:46 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+(async () => {
+
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.jwst.nasa.gov/content/webbLaunch/whereIsWebb.html');
+
+    setInterval(async function(){
+        const data = await page.evaluate(() => {
+            function getText(element, dataType) {
+                let data = document.querySelector(element).textContent;
+                if(dataType === 'float') data=parseFloat(data);
+                return data;
+            };
+            
+            let earthDistance = getText('#milesEarth', 'float');
+            let l2Distance = getText('#milesToL2', 'float');
+            let percentageDistance = getText('#percentageCompleted', 'float')
+            let speedMi = getText('#speedMi', 'float')
+            let data = {
+                earthDistance: earthDistance,
+                l2Distance: l2Distance,
+                percentageDistance: percentageDistance,
+                cruisingSpeed: speedMi
+            }           
+            return JSON.stringify(data, null, 2);
+        });        
+        fs.writeFileSync('data.json', data);
+    }, 2000);
+
+})();
